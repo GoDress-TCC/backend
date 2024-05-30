cd8849a95207a07aae0002c99df10f14cfd49166
Lucas Pedro Chaves <lucaspedrosupergeeks@gmail.com>
2024-05-30 11:53:51 -0300

    Conexão com o banco MongoDB Atlas

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
+
 app.use(cors())
 app.use(express.json())
+
+// Conexão com o banco de dados
+const connection = require('../db/conn')
+
+connection();
 
 const port = 3000
 
@@ -15,4 +21,3 @@ app.listen(port, () => {
     console.log(`App rodando na porta ${port}`)
 })      
 
-    
