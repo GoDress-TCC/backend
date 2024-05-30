c97997871870451a7ef8f07947ea9b395ec8efe3
Lucas Pedro Chaves <lucaspedrosupergeeks@gmail.com>
2024-05-30 11:31:23 -0300

    Arquitetura de pastas, inserção do cors no index.js, e setup do nodemon

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('App rodando')
+    res.send('App rodando!')
 })
 
 app.listen(port, () => {
