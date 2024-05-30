dcd003c15ac2851d6788cdd07360efa7a241b578
Lucas Pedro Chaves <lucaspedrosupergeeks@gmail.com>
2024-05-30 11:16:04 -0300

    Novos pacotes instalados: mongoose, cors e nodemon

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
+
 const app = express()
 const port = 3000
 
-
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('App rodando')
 })
 
 app.listen(port, () => {
     console.log(`App rodando na porta ${port}`)
 })      
 
+    
