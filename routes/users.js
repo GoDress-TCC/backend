2988c93d5d3d2143f5815e9f60f09c3978f936b5
Lucas Pedro Chaves <lucaspedrosupergeeks@gmail.com>
2024-06-05 16:39:10 -0300

    Endpoint de registro de usuários criado no banco de dados

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 const userController = require('../controllers/userController')
 
+router.route('/auth/register').post((req, res) => userController.create(req, res))
+
 module.exports = router;
 
