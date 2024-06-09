2824846c62465a1620243c75c9bbe50d4d76265e
Gttolla <lucaspedrosupergeeks@gmail.com>
2024-06-09 14:20:02 -0300

    criptografia de senha com bcrypt e checagem de usuários já cadastrados

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,10 @@
 const { response } = require('express');
 const { User: userModel } = require('../models/User')
+const bcrypt = require('bcrypt')
 
 const userController = {
-    create: async(req, res) => {
-        try{
+    create: async (req, res) => {
+        try {
             const user = {
                 name: req.body.name,
                 surname: req.body.surname,
@@ -11,19 +12,21 @@ const userController = {
                 password: req.body.password,
                 age: req.body.age,
             }
-            
-            const userExists = await userModel.findOne({ email: email })
 
-            if(userExists) {
-                res.status(422).json({ msg: "Este email já está cadastrado" })
+            const userExists = await userModel.findOne({ email: user.email })
+
+            if (userExists) {
+                return res.status(422).json({ msg: "Este email já está cadastrado" })
             }
 
+            // criptografia de senha
+            const salt = await bcrypt.genSalt(12)
+            user.password = await bcrypt.hash(user.password, salt)
             
-
-            // const response = await userModel.create(user);
-            // res.status(201).json({ response, msg: "Usuário criado com sucesso!" });
+            const response = await userModel.create(user);
+            res.status(201).json({ response, msg: "Usuário criado com sucesso!" });
         }
-        catch(error){
+        catch (error) {
             res.status(400).json({ msg: error.message })
         }
     }
