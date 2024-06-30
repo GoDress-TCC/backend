22105f98100eabe51fab8e54be2fe5393b4d4b61
Lucas Pedro Chaves <lucaspedrosupergeeks@gmail.com>
2024-06-30 13:29:36 -0300

    Inicio do desenvolvimento dos CRUDs do projeto/Models: Cat, Clothe, Event, Outfit, criados!

---
+++ models/Clothe.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const clotheSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    catId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Cat'
+    },
+    name: {
+        type: String,
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: String,
+    },
+    fit: {
+        type: String,
+    },
+    gender: {
+        type: String
+    }
+}, { timestamps: true });
+
+const Clothe = mongoose.model('Clothe', clotheSchema)
+
+module.exports = {
+    Clothe,
+    clotheSchema,
+};
